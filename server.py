4f80e495ee8fdb14a44a2b3b7f00047033a390c4
vedant0302 <vedantgawand04@gmail.com>
2026-04-06 23:30:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -320,7 +320,7 @@ def health():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     print(f"🚀 QuickCart backend → http://0.0.0.0:{port}")
     app.run(debug=False, host="0.0.0.0", port=port)
     #port = int(os.environ.get("PORT", 5000))
